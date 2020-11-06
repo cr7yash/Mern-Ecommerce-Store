8d635df53d6a18b84f0841acb266e20ed797fa6a
Amadeus96 <yashaqua@gmail.com>
2020-11-07 01:00:17 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image: '/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'2',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image: '/images/p2.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'3',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image: '/images/p3.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        }
+    ]
+}
+
+export default data
