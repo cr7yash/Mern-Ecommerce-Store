92303d3d86629183b6d33517b2256e003447590a
Amadeus96 <yashaqua@gmail.com>
2020-11-16 20:34:44 +0530

    Create sample users in MongoDB

--- Backend/data.js
+++ Backend/data.js
@@ -1,39 +1,61 @@
-const data = {
-    products:[
-        {
-            _id:'1',
-            name:'Nike Slim Shirt',
-            category:'Shirts',
-            image: '/images/p1.jpg',
-            price:120,
-            brand:'Nike',
-            rating:4.5,
-            numReviews:10,
-            description:'High quality product'
-        },
-        {
-            _id:'2',
-            name:'Nike Slim Shirt',
-            category:'Shirts',
-            image: '/images/p2.jpg',
-            price:120,
-            brand:'Nike',
-            rating:4.5,
-            numReviews:10,
-            description:'High quality product'
-        },
-        {
-            _id:'3',
-            name:'Nike Slim Shirt',
-            category:'Shirts',
-            image: '/images/p3.jpg',
-            price:120,
-            brand:'Nike',
-            rating:4.5,
-            numReviews:10,
-            description:'High quality product'
-        }
-    ]
-}
+import bcrypt from 'bcrypt';
 
-export default data
+const data = {
+	users: [
+		{
+			name: 'Yash',
+			email: 'yashxyz@gmail.com',
+			password: bcrypt.hashSync('123456', 8),
+			isAdmin: true,
+		},
+		{
+			name: 'anan',
+			email: 'anan@gmail.com',
+			password: bcrypt.hashSync('123456', 8),
+			isAdmin: false,
+		},
+		{
+			name: 'anany',
+			email: 'anany@gmail.com',
+			password: bcrypt.hashSync('123456', 8),
+			isAdmin: false,
+		},
+	],
+	products: [
+		{
+			_id: '1',
+			name: 'Nike Slim Shirt',
+			category: 'Shirts',
+			image: '/images/p1.jpg',
+			price: 120,
+			brand: 'Nike',
+			rating: 4.5,
+			numReviews: 10,
+			description: 'High quality product',
+		},
+		{
+			_id: '2',
+			name: 'Nike Slim Shirt',
+			category: 'Shirts',
+			image: '/images/p2.jpg',
+			price: 120,
+			brand: 'Nike',
+			rating: 4.5,
+			numReviews: 10,
+			description: 'High quality product',
+		},
+		{
+			_id: '3',
+			name: 'Nike Slim Shirt',
+			category: 'Shirts',
+			image: '/images/p3.jpg',
+			price: 120,
+			brand: 'Nike',
+			rating: 4.5,
+			numReviews: 10,
+			description: 'High quality product',
+		},
+	],
+};
+
+export default data;
--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,47 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
 
+mongoose.connect(
+	process.env.MONGODB_URL ||
+		'mongodb+srv://yashaqua:MMxYFFu7PKqp4l50@cluster0.ytuke.mongodb.net/<dbname>?retryWrites=true&w=majority',
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	}
+);
+
+app.get('/api/products/:id', (req, res) => {
+	const product = data.products.find((x) => x._id === req.params.id);
+	if (product) {
+		res.send(product);
+	} else {
+		res.status(404).send({ message: 'Product Not Found' });
+	}
+});
+
+app.get('/api/products', (req, res) => {
+	res.send(data.products);
+});
+
+app.use('/api/users', userRouter);
+app.get('/', (req, res) => {
+	res.send('Server is ready');
+});
+
 app.get('/', (req, res) => {
 	res.send('Hi backend');
+});
+
+app.use((err, req, res, next) => {
+	res.status(500).send({ message: err.message });
+});
+
+app.use((err, req, res, next) => {
+	res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
