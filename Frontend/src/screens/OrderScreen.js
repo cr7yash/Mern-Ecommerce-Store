2f9e5c2d3b887742e0db07eb6f476e3056da4ce7
cr7yash <yashaqua@gmail.com>
2021-02-25 19:24:03 +0530

    Adding the paypal button

--- Frontend/src/screens/OrderScreen.js
+++ Frontend/src/screens/OrderScreen.js
@@ -1,19 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { detailsOrder } from '../actions/orderActions';
+import Axios from 'axios';
+import { PayPalButton } from 'react-paypal-button-v2';
 
 export default function OrderScreen(props) {
 	const orderId = props.match.params.id;
+	const [sdkReady, setSdkReady] = useState(false);
 	const orderDetails = useSelector((state) => state.orderDetails);
 	const { order, loading, error } = orderDetails;
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		dispatch(detailsOrder(orderId));
-	}, [dispatch, orderId]);
+		const addPayPalScript = async () => {
+			const { data } = await Axios.get('/api/config/paypal');
+			const script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.src = `https://www.paypal.com/sdk/js?client-id=${data}`;
+			script.async = true;
+			script.onload = () => {
+				setSdkReady(true);
+			};
+			document.body.appendChild(script);
+		};
+		if (!order._id) {
+			dispatch(detailsOrder(orderId));
+		} else {
+			if (!order.isPaid) {
+				if (!window.paypal) {
+					addPayPalScript();
+				} else {
+					setSdkReady(true);
+				}
+			}
+		}
+	}, [dispatch, order, sdkReady, orderId]);
+
+	const successPaymentHandler = () => {
+		//Todo
+	};
 
 	return loading ? (
 		<LoadingBox></LoadingBox>
@@ -136,22 +164,31 @@ export default function OrderScreen(props) {
 									</div>
 								</div>
 							</li>
-							{/* <li>
-								<button
-									type='button'
-									onClick={placeOrderHandler}
-									className='primary block'
-									disabled={cart.cartItems.length === 0}
-								>
-									Place Order
-								</button>
-							</li> */}
-							{/* {loading && <LoadingBox></LoadingBox>}
-							{error && (
-								<MessageBox variant='danger'>
-									{error}
-								</MessageBox>
-							)} */}
+							{!order.isPaid && (
+								<li>
+									{!sdkReady ? (
+										<LoadingBox></LoadingBox>
+									) : (
+										<>
+											{errorPay && (
+												<MessageBox variant='danger'>
+													{errorPay}
+												</MessageBox>
+											)}
+											{loadingPay && (
+												<LoadingBox></LoadingBox>
+											)}
+
+											<PayPalButton
+												amount={order.totalPrice}
+												onSuccess={
+													successPaymentHandler
+												}
+											></PayPalButton>
+										</>
+									)}
+								</li>
+							)}
 						</ul>
 					</div>
 				</div>
